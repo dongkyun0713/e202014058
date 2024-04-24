2c9c50c7f78d6abb5208097d946031b31fdb93b9
dongkyunKim <kyun010713@gmail.com>
2024-04-24 12:59:50 +0900

    fourth

--- src/World.java
+++ src/World.java
@@ -1,1 +1,1 @@
-public class World { public static void run() { System.out.println("안녕하세요"); } }
+public class World { public static void run() { System.out.println("안녕하세요"); System.out.println("홍길동"); } }
